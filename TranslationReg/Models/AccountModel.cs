a1069d217037b9f2f5fe0c133d78d351e838a448
Dischain1 <Dischain123321@gmail.com>
2018-03-05 18:06:16 +0700

    user login regular expression pattern and other login model restrinctions were added. necessarry fields were marked. fields aliaces added

--- TranslationReg/Models/AccountModel.cs
+++ TranslationReg/Models/AccountModel.cs
@@ -19,13 +19,19 @@ namespace TranslationReg.Models
     public class RegisterModel
     {
         [Required]
+        [Display(Name = "\"Логин\"")]
+        [StringLength(100, ErrorMessage = "Логин должен включать менее 100 символов")]
+        [RegularExpression(@"^[a-zA-Zа-яА-Я0-9 _]+$", ErrorMessage = "Пожалуйста, используйте только буквы")]
         public string Name { get; set; }
 
         [Required]
+        [StringLength(100, ErrorMessage = "Пароль должен включать менее 100 символов")]
         [DataType(DataType.Password)]
+        [Display (Name = "\"Пароль\"")]
         public string Password { get; set; }
 
         [Required]
+        [Display(Name = "\"Подтверждение пароля\"")]
         [DataType(DataType.Password)]
         [Compare("Password", ErrorMessage = "Пароли не совпадают")]
         public string ConfirmPassword { get; set; }

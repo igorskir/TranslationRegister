91f061de5cdac87fa62370848949ad7c69f2f168
Dischain1 <Dischain123321@gmail.com>
2018-03-11 00:22:02 +0700

    CRUD languages cards hotfix

--- TranslationReg/Scripts/scripts.js
+++ TranslationReg/Scripts/scripts.js
@@ -16,6 +16,23 @@
         });
     });
 
+    $(".ajaxToggleCard").one('click', function (e) {
+        updatedBlock = $(this).attr("data-ajax-update");
+        myurl = $(this).attr("data-href");
+        $.ajax({
+            type: 'GET',
+            url: myurl,
+            success: function (result) {
+                $(updatedBlock).html(result).hide();
+                $(updatedBlock).toggle();
+                $(e.target).on("click", function () {
+                    var updatedBlock = $(this).attr("data-ajax-update");
+                    $(updatedBlock).toggle();
+                });
+            }
+        });
+    });
+
     $(".filter").click(function () {
         $(".filter").removeClass("filter-active");
         $(this).addClass("filter-active");
